b1287ea5c535cc20610ba832dd12e18c7ea09625
Shivam Kushwaha <svm.kushwaha@gmail.com>
2024-05-20 20:38:02 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
+import Navigation from "./Navigation";
+import { WithSplashScreen } from "./WithSplashScreen";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <WithSplashScreen isAppReady={true}>
+        <Navigation />
+      </WithSplashScreen>
+    </Provider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
